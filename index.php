56b3de7ac91e092e9fe80fd106930c9309f9c834
keanallen <keanallenn@gmail.com>
2020-10-27 16:45:13 +0800

    commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>We are the HYPE!!!</title>
+    <title>We are the HYPE!</title>
     <link rel="shortcut icon" href="img/logo.svg" type="image/svg">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
