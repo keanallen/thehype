5a7515c6fbe9a8e890597a7632472a53165a53ad
keanallen <keanallenn@gmail.com>
2020-10-27 21:16:08 +0800

    commit

--- index.php
+++ index.php
@@ -303,6 +303,28 @@
                 </div>
             </section>
 
+            <section id="clients">
+                <div class="container">
+                    <div class="row my-5">
+                        <div class="col-lg-12">
+                            <h1 class="hype-txt-bold text-center">Our Clients</h1>
+                            <section class="customer-logos my-5 slider">
+                                <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
+                                <div class="slide"><img src="http://www.webcoderskull.com/img/logo.png"></div>
+                                <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
+                                <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
+                                <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
+                                <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
+                                <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
+                                <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
+                                <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
+                            </section>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
             <section id="contact">
 
                 <div class="container">
@@ -505,6 +527,27 @@
             dots: true,
             centerMode: true,
             focusOnSelect: true
+        });
+
+        old_jquery('.customer-logos').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: false,
+            dots: false,
+            pauseOnHover: false,
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4
+                }
+            }, {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 3
+                }
+            }]
         });
 
         $("p.copytext").html(`&copy; Copyright ${new Date().getFullYear()} - <b>The Hype</b>. All rights
